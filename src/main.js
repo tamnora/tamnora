25cf887b9b75f78ab3117f995e195d6ada27be76
Daniel Fuentes <tamnora@hotmail.com>
2023-08-10 17:28:14 -0300

    nuevo elementos a ID

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 import Tamnora from './js/tamnora.js';
 const tmn = new Tamnora();
+tmn.setData('contador', 0);
 
-tmn.id('btnToggle').click(function() {
 
-  // if set via local storage previously
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+    document.documentElement.classList.add('dark');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+    document.documentElement.classList.remove('dark');
+}
+
+
+
+tmn.id('theme-toggle').click(function() {
   if (localStorage.getItem('color-theme')) {
       if (localStorage.getItem('color-theme') === 'light') {
           document.documentElement.classList.add('dark');
           localStorage.setItem('color-theme', 'dark');
-          
+          themeToggleLightIcon.classList.remove('hidden');
+          themeToggleDarkIcon.classList.add('hidden');
       } else {
           document.documentElement.classList.remove('dark');
           localStorage.setItem('color-theme', 'light');
-          
+          themeToggleDarkIcon.classList.remove('hidden');
+          themeToggleLightIcon.classList.add('hidden');
       }
 
   // if NOT set via local storage previously
@@ -20,18 +36,23 @@ tmn.id('btnToggle').click(function() {
       if (document.documentElement.classList.contains('dark')) {
           document.documentElement.classList.remove('dark');
           localStorage.setItem('color-theme', 'light');
-          
+          themeToggleDarkIcon.classList.remove('hidden');
+          themeToggleLightIcon.classList.add('hidden');
       } else {
           document.documentElement.classList.add('dark');
+          themeToggleLightIcon.classList.remove('hidden');
+          themeToggleDarkIcon.classList.add('hidden');
           localStorage.setItem('color-theme', 'dark');
-          
       }
   }
-  
 })
 
 
+tmn.id('myButton').click(()=>{
 
+    tmn.data.contador++
+    tmn.id('contador').html(`Valor en ${tmn.data.contador}`)
+})
 
 tmn.onMount(() => {
   console.log('¡tamnora.js está listo en index!');
