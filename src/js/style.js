ebcbfa1318b4ec4eb681d564cb884608a532d2ab
Daniel Fuentes <tamnora@hotmail.com>
2023-08-20 18:36:30 -0300

    caminando por un div

--- src/js/style.js
+++ src/js/style.js
@@ -2,7 +2,7 @@ const styleClass = {
   label: 'block text-gray-400 text-sm pl-2 font-medium',
   navlink: "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent",
   input: "bloc w-full bg-gray-800 text-white placeholder:text-gray-600 px-4 py-2 rounded focus:ring focus:outline-none focus:ring-blue-800",
-  btn: "h-10 font-medium rounded-lg px-4 py-2 text-sm focus:ring focus:outline-none",
+  btn: "h-10 font-medium rounded-lg px-4 py-2 text-sm focus:ring focus:outline-none transition-bg duration-500",
   darkBlue: "bg-blue-700 text-white hover:bg-blue-800 focus:ring-blue-700",
   darkRed: "bg-red-700 text-white hover:bg-red-800 focus:ring-red-700",
   darkGreen: "bg-green-700 text-white hover:bg-green-800 focus:ring-green-700",
