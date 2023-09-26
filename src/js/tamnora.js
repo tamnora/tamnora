ac4afa0e49428bc58debd77c38c3efc69f07a786
Tamnora <tamnora@hotmail.com>
2023-09-26 18:44:34 -0300

    Compatibilidad con nodejs

--- src/js/tamnora.js
+++ src/js/tamnora.js
@@ -1,4 +1,5 @@
-const SERVER = import.meta.env.VITE_SERVER_PROD;
+const SERVER = import.meta.env.VITE_SERVER_DEV;
+const TYPE_SERVER = 'php';
 
 let informe = {primero: 'nada', segundo:'nada'};
 
@@ -277,12 +278,25 @@ export async function dbSelect(type, sql) {
 
 
   try {
-    const resp = await fetch(`${SERVER}/tsql.php`, {
-      method: 'POST',
-      body: JSON.stringify({
-        data: datos
-      })
-    });
+    let resp;
+    if(TYPE_SERVER == 'php'){
+      resp = await fetch(`${SERVER}/tsql.php`, {
+        method: 'POST',
+        body: JSON.stringify({
+          data: datos
+        })
+      });
+    } else {
+      resp = await fetch(`${SERVER}/tsql`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: datos
+        })
+      });
+    }
 
     const result = await resp.json();
     return result;
@@ -301,12 +315,25 @@ export async function login(user, password) {
   };
 
   try {
-    const resp = await fetch(`${SERVER}/login.php`, {
+    let resp;
+    if(TYPE_SERVER == 'php'){
+    resp = await fetch(`${SERVER}/login.php`, {
       method: 'POST',
       body: JSON.stringify({
         data: datos
       })
     });
+    } else {
+      resp = await fetch(`${SERVER}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: datos
+        })
+      });
+    }
 
     const result = await resp.json();
     return result;
@@ -323,12 +350,25 @@ export async function structure(type, name) {
   };
 
   try {
-    const resp = await fetch(`${SERVER}/structure.php`, {
-      method: 'POST',
-      body: JSON.stringify({
-        data: datos
-      })
-    });
+    let resp;
+    if(TYPE_SERVER == 'php'){
+      resp = await fetch(`${SERVER}/structure.php`, {
+        method: 'POST',
+        body: JSON.stringify({
+          data: datos
+        })
+      });
+    } else {
+      resp = await fetch(`${SERVER}/struc`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          data: datos
+        })
+      });
+    }
 
     const result = await resp.json();
     return result;
@@ -2320,6 +2360,7 @@ export class DataObject {
 
             if (send.status == 1) {
               dbSelect(send.tipo, send.sql).then(val => {
+                console.log(val)
                 if(val[0].resp == 1){
                   resolve(val[0].msg);
                 } else {
@@ -2956,6 +2997,10 @@ export class DataObject {
         let value = dataObject[fieldName];
         let type = this.detectDataType(value);
         let key = '';
+
+        if(type == 'datetime'){
+          value = this.formatDate(new Date(value)).fechaHora;
+        }
         if (clean == true) {
           if (type == 'number') {
             value = 0;
@@ -3693,6 +3738,8 @@ export class DataObject {
   detectDataType(value) {
     if (!isNaN(parseFloat(value)) && isFinite(value)) {
       return "number";
+    } else if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(value)) {
+      return "datetime";
     } else if (/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/.test(value)) {
       return "datetime-local";
     } else if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
@@ -4600,6 +4647,11 @@ export class DataArray {
         let value = dataObject[fieldName];
         let type = this.detectDataType(value);
         let key = '';
+
+        if(type == 'datetime'){
+          value = this.formatDate(new Date(value)).fechaHora;
+        }
+
         if (clean == true) {
           if (type == 'number') {
             value = 0;
@@ -4738,6 +4790,8 @@ export class DataArray {
   detectDataType(value) {
     if (!isNaN(parseFloat(value)) && isFinite(value)) {
       return "number";
+    } else if (/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(value)) {
+      return "datetime";
     } else if (/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/.test(value)) {
       return "datetime-local";
     } else if (/^\d{4}-\d{2}-\d{2}$/.test(value)) {
@@ -5325,6 +5379,139 @@ export class DataArray {
       return `${numeroString}`;
     }
   }
+
+
+formatDate(valor = null, separador = '-') {
+  let fechaHora;
+  let myDate;
+  let sep = separador || '-';
+  if (valor == null) {
+    valor = new Date();
+    myDate = valor;
+  }
+
+  let exp = /^\d{2,4}\-\d{1,2}\-\d{1,2}\s\d{1,2}\:\d{1,2}\:\d{1,2}$/gm;
+  const arrayDias = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  const arrayDia = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
+  const arrayMeses = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+  ];
+  const arrayMes = [
+    'Ene',
+    'Feb',
+    'Mar',
+    'Abr',
+    'May',
+    'jun',
+    'Jul',
+    'Ago',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dic'
+  ];
+
+  if (typeof valor == 'string') {
+    if (valor.match(exp)) {
+      fechaHora = valor;
+      myDate = new Date(valor);
+    } else {
+      return '';
+    }
+  }
+
+  if (typeof valor == 'object') {
+    myDate = valor;
+  }
+
+  if (typeof valor !== 'string' && typeof valor !== 'object') {
+    return 'parametro incorrecto';
+  }
+
+  let anio = myDate.getFullYear();
+  let mes = myDate.getMonth() + 1;
+  let dia = myDate.getDate();
+  let dsem = myDate.getDay();
+  let hora = myDate.getHours();
+  let minutos = myDate.getMinutes();
+  let segundos = myDate.getSeconds();
+
+  mes = mes < 10 ? '0' + mes : mes;
+  dia = dia < 10 ? '0' + dia : dia;
+  hora = hora < 10 ? '0' + hora : hora;
+  minutos = minutos < 10 ? '0' + minutos : minutos;
+  segundos = segundos < 10 ? '0' + segundos : segundos;
+
+  let myObject = {
+    fecha: '' + myDate.getFullYear() + '-' + mes + '-' + dia,
+    fechaEs: '' + dia + sep + mes + sep + myDate.getFullYear(),
+    anio: myDate.getFullYear(),
+    mes: mes,
+    mesCorto: arrayMes[myDate.getMonth()],
+    mesLargo: arrayMeses[myDate.getMonth()],
+    dia: dia,
+    diaSem: dsem,
+    anioMes: anio + sep + mes,
+    mesDia: mes + sep + dia,
+    diaCorto: arrayDia[dsem],
+    diaLargo: arrayDias[dsem],
+    fechaCarta:
+      arrayDias[dsem] +
+      ' ' +
+      myDate.getDate() +
+      ' de ' +
+      arrayMeses[myDate.getMonth()] +
+      ' de ' +
+      myDate.getFullYear(),
+    fechaTonic:
+      '' + myDate.getDate() + sep + arrayMes[myDate.getMonth()] + sep + myDate.getFullYear(),
+    fechaHoraEs:
+      '' +
+      dia +
+      sep +
+      mes +
+      sep +
+      myDate.getFullYear() +
+      ' ' +
+      hora +
+      ':' +
+      minutos +
+      ':' +
+      segundos,
+    fechaHora:
+      '' +
+      myDate.getFullYear() +
+      '-' +
+      mes +
+      '-' +
+      dia +
+      ' ' +
+      hora +
+      ':' +
+      minutos +
+      ':' +
+      segundos,
+    fechaHoraT: '' + myDate.getFullYear() + '-' + mes + '-' + dia + 'T' + hora + ':' + minutos,
+    horaLarga: hora + ':' + minutos + ':' + segundos,
+    horaCorta: hora + ':' + minutos,
+    hora: hora,
+    minutos: minutos,
+    segundos: segundos
+  };
+
+  return myObject;
+}
 
 
   objectColorTailwind(){
