82a15b5000837ec2b8e2ab6734ef68de870eca5c
Tamnora <tamnora@hotmail.com>
2023-10-05 12:32:32 -0300

    Anexo de dmeo

--- src/js/tamnora.js
+++ src/js/tamnora.js
@@ -454,7 +454,7 @@ export class Tamnora {
     this.colorPrimary = 'zinc';
     this._styleClasses = config.styleClasses || {
       label: `block pl-1 text-sm font-medium text-zinc-900 dark:text-zinc-400`,
-      navlink: `block py-2 pl-3 pr-4 text-zinc-900 rounded hover:bg-zinc-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent`,
+      navlink: `block  text-zinc-900 rounded hover:bg-zinc-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-zinc-700 dark:hover:text-white md:dark:hover:bg-transparent`,
       input: `bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:outline-none  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-zinc-800 dark:border-zinc-700 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-700 dark:focus:border-blue-700`,
       select: `bg-zinc-50 border border-zinc-300 text-zinc-900 text-sm rounded-lg focus:outline-none  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-zinc-800 dark:border-zinc-700 dark:placeholder-zinc-400 dark:text-white dark:focus:ring-blue-700 dark:focus:border-blue-700`,
       btn: `h-10 font-medium rounded-lg px-4 py-2 text-sm focus:ring focus:outline-none transition-bg duration-500`,
@@ -474,7 +474,7 @@ export class Tamnora {
       btnDanger: `bg-red-700 text-white hover:bg-red-800 focus:ring-red-700`,
       btnSuccess: `bg-green-700 text-white hover:bg-green-800 focus:ring-green-700`,
       btnSecondary: `bg-zinc-700 text-white hover:bg-zinc-800 focus:ring-zinc-700`,
-      dark: `bg-zinc-300 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 dark:bg-zinc-800 dark:text-zinc-100 hover:dark:bg-zinc-700 hover:dark:text-white focus:ring-zinc-700`,
+      btndark: `bg-zinc-300 text-zinc-800 hover:bg-zinc-100 hover:text-zinc-900 dark:bg-zinc-800 dark:text-zinc-100 hover:dark:bg-zinc-700 hover:dark:text-white focus:ring-zinc-700`,
       navactive: `text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500`,
       inactive: `text-zinc-600`,
     };
@@ -1075,7 +1075,6 @@ export class Tamnora {
         componentDivs.push(element);
       }
     }
-
 
 
     const cantComponents = componentDivs.length;
